8255525647031a398f1e9138c204f852c4b2f26d
Gabriel Lee <gabriel.lee@central1.com>
2020-02-05 19:33:54 -0800

    Add Python convention comments

--- trbruteforce.py
+++ trbruteforce.py
@@ -13,9 +13,11 @@ def main():
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-c', '--ciphertext', help='the ciphertext to decode')
     group.add_argument('-i', '--input', help='name of the input text file')
+    # TODO: Implement output to file
     parser.add_argument('-o', '--output', help='name of the output text file')
     args = parser.parse_args()
 
+    # Handle command line or file logic
     if args.input is not None:
         ct_file = open(args.input, "r")
         for line in ct_file:
@@ -24,6 +26,13 @@ def main():
         brute_force_transpose(args.ciphertext)
 
 def brute_force_transpose(ct):
+    """
+    Attempt to break the ciphertext by performing transposition decryption cipher from length 1 to ciphertext length.
+    Check at each iteration the human readability threshold.
+    If threshold is met, prompt user verification.
+
+    ct - the ciphertext
+    """
     for i in range(1, len(ct)+1):
         print(i)
         decrypted = decryptMessage(i, ct)
